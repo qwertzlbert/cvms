1b76e4a5e0e238b1c67531b8193474733f2fd0f8
Jeongseup <seup87@gmail.com>
2024-11-22 16:48:48 +0900

    Update GetConfig for if not existed custom_chains.yaml

--- internal/helper/config/support_chain.go
+++ internal/helper/config/support_chain.go
@@ -31,32 +31,41 @@ func GetSupportChainConfig() (*SupportChains, error) {
 		return nil, errors.Wrapf(err, "failed to read config file")
 	}
 
-	ctDataBytes, err := os.ReadFile(MustGetSupportChainPath("custom_chains.yaml"))
-	if err != nil {
-		return nil, errors.Wrapf(err, "failed to read config file")
-	}
-
 	scCfg := &SupportChains{}
 	err = yaml.Unmarshal(dataBytes, scCfg)
 	if err != nil {
 		return nil, errors.Wrapf(err, "failed to decode config file")
 	}
 
-	ctCfg := &SupportChains{}
-	err = yaml.Unmarshal(ctDataBytes, ctCfg)
-	if err != nil {
-		return nil, errors.Wrapf(err, "failed to decode second config file")
-	}
+	if fileExists(MustGetSupportChainPath("custom_chains.yaml")) {
+		ctDataBytes, err := os.ReadFile(MustGetSupportChainPath("custom_chains.yaml"))
+		if err != nil {
+			return nil, errors.Wrapf(err, "failed to read config file")
+		}
+		ctCfg := &SupportChains{}
+		err = yaml.Unmarshal(ctDataBytes, ctCfg)
+		if err != nil {
+			return nil, errors.Wrapf(err, "failed to decode second config file")
+		}
+		// Merge the two configurations
+		for chainName, chainDetail := range ctCfg.Chains {
+			// NOTE: this will override chain config for users to use custom_chains.yaml
+			if _, exists := scCfg.Chains[chainName]; exists {
+				scCfg.Chains[chainName] = chainDetail
+			}
 
-	// Merge the two configurations
-	for chainName, chainDetail := range ctCfg.Chains {
-		// NOTE: this will override chain config for users to use custom_chains.yaml
-		if _, exists := scCfg.Chains[chainName]; exists {
+			// Add custom chains by custom_chains.yaml
 			scCfg.Chains[chainName] = chainDetail
 		}
-
-		// Add custom chains by custom_chains.yaml
-		scCfg.Chains[chainName] = chainDetail
 	}
+
 	return scCfg, nil
+}
+
+func fileExists(path string) bool {
+	_, err := os.Stat(path)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return err == nil
 }
