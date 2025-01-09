64c0a740e5402567b08649c74bc13b497e652eea
Simon Lichtenauer <Simon.Lichtenauer@figment.io>
2025-01-09 20:32:24 +0100

    replace deprecated grpc and protobuf libs
    Signed-off-by: Simon Lichtenauer <Simon.Lichtenauer@figment.io>

--- internal/helper/grpc/grpc_query.go
+++ internal/helper/grpc/grpc_query.go
@@ -2,32 +2,22 @@ package grpchelper
 
 import (
 	"context"
-
-	"github.com/golang/protobuf/jsonpb"
-	"github.com/jhump/protoreflect/desc"
-	"github.com/jhump/protoreflect/dynamic/grpcdynamic"
-	"github.com/pkg/errors"
+	"strings"
 
 	"log"
+
+	"github.com/pkg/errors"
 
 	"github.com/jhump/protoreflect/grpcreflect"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/metadata"
+	"google.golang.org/protobuf/encoding/protojson"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/reflect/protoreflect"
+	"google.golang.org/protobuf/types/dynamicpb"
 )
 
-var (
-	jsonPbMarshaler = jsonpb.Marshaler{
-		OrigName:     true,
-		EmitDefaults: true,
-		AnyResolver:  DynamicAnyResolver{},
-	}
-
-	jsonPbUnmarshaler = jsonpb.Unmarshaler{
-		AllowUnknownFields: true,
-	}
-)
-
-func GrpcMakeDescriptor(reflectionClient *grpcreflect.Client, queryPath string) (*desc.MethodDescriptor, error) {
+func GrpcMakeDescriptor(reflectionClient *grpcreflect.Client, queryPath string) (protoreflect.MethodDescriptor, error) {
 	methodDescriptor, err := ResolveMessage(queryPath, reflectionClient)
 	if err != nil {
 		return nil, errors.Wrapf(err, "by query path: %s", queryPath)
@@ -38,71 +28,95 @@ func GrpcMakeDescriptor(reflectionClient *grpcreflect.Client, queryPath string) 
 
 func GrpcInvokeQuery(
 	ctx context.Context,
-	methodDescriptor *desc.MethodDescriptor,
-	stub *grpcdynamic.Stub,
+	methodDescriptor protoreflect.MethodDescriptor,
+	conn *grpc.ClientConn,
 	queryData string,
 ) (string, error) {
 
-	msg, err := CreateMessage(methodDescriptor, &jsonPbUnmarshaler, queryData)
+	msgRaw, err := CreateMessage(methodDescriptor, queryData)
 	if err != nil {
 		// client.Logger.Errorf("grpc api failed to create proto message: %v", err)
 		return "", err
 	}
+	var msg []byte
+	err = proto.Unmarshal(msg, msgRaw)
 
-	_, err = msg.MarshalJSONPB(&jsonPbMarshaler)
 	if err != nil {
 		// client.Logger.Errorf("grpc api failed to mashal jsonpb: %v", err)
 		return "", err
 	}
-
+	response := dynamicpb.NewMessage(methodDescriptor.Output())
 	var headerMD metadata.MD
-
-	resp, err := stub.InvokeRpc(context.Background(), methodDescriptor, msg, grpc.Header(&headerMD))
+	err = conn.Invoke(ctx, string(msgRaw.Descriptor().FullName()), msg, response, grpc.Header(&headerMD))
 	if err != nil {
 		// client.Logger.Errorf("grpc api failed to invoke rpc: %v", err)
 		return "", err
 	}
 
-	respJSON, err := jsonPbMarshaler.MarshalToString(resp)
+	var resolver CosmosAnyMessageResolver
+
+	marshaller := protojson.MarshalOptions{
+		AllowPartial:    true,
+		Indent:          "  ",
+		UseProtoNames:   false,
+		EmitUnpopulated: false,
+		Resolver:        resolver,
+	}
+	respJSON, err := marshaller.Marshal(response)
 	if err != nil {
 		// client.Logger.Errorf("grpc api failed to marshal string with grpc data: %v", err)
 		return "", err
 	}
 
-	return respJSON, nil
+	return string(respJSON), nil
 }
 
-func GrpcDynamicQuery(ctx context.Context, reflectionClient *grpcreflect.Client, stub *grpcdynamic.Stub, queryPath string, queryData string) (string, error) {
+func GrpcDynamicQuery(
+	ctx context.Context,
+	reflectionClient *grpcreflect.Client,
+	conn *grpc.ClientConn,
+	queryPath string,
+	queryData string,
+) (string, error) {
 	methodDescriptor, err := ResolveMessage(queryPath, reflectionClient)
 	if err != nil {
 		log.Printf("grpc api failed to resolve proto message: %s", err.Error())
 		return "", err
 	}
 
-	msg, err := CreateMessage(methodDescriptor, &jsonPbUnmarshaler, queryData)
+	// Creates proto message body from query data formated in json
+	msg, err := CreateMessage(methodDescriptor, queryData)
 	if err != nil {
 		log.Printf("grpc api failed to create proto message: %s", err.Error())
 		return "", err
 	}
 
-	_, err = msg.MarshalJSONPB(&jsonPbMarshaler)
-	if err != nil {
-		log.Printf("grpc api failed to mashal jsonpb: %s", err.Error())
-		return "", err
-	}
+	idx := strings.LastIndex(queryPath, ".")
+	fullMethodName := queryPath[:idx] + "/" + queryPath[idx+1:]
+
+	response := dynamicpb.NewMessage(methodDescriptor.Output())
 
 	var headerMD metadata.MD
-	resp, err := stub.InvokeRpc(ctx, methodDescriptor, msg, grpc.Header(&headerMD))
+	err = conn.Invoke(ctx, fullMethodName, msg, response, grpc.Header(&headerMD))
 	if err != nil {
-		log.Printf("grpc api failed to invoke rpc: %s", resp)
+		log.Printf("grpc api failed to invoke rpc: %s", err)
 		return "", err
 	}
 
-	respJSON, err := jsonPbMarshaler.MarshalToString(resp)
+	var resolver CosmosAnyMessageResolver
+
+	marshaller := protojson.MarshalOptions{
+		AllowPartial:    true,
+		Indent:          "  ",
+		UseProtoNames:   false,
+		EmitUnpopulated: false,
+		Resolver:        resolver,
+	}
+	respJSON, err := marshaller.Marshal(response)
 	if err != nil {
 		log.Printf("grpc api failed to marshal string with grpc data: %s", err.Error())
 		return "", err
 	}
 
-	return respJSON, nil
+	return string(respJSON), nil
 }
