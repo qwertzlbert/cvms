861a495226c8301fc57fb9e210174f1fb7b63d67
Simon Lichtenauer <Simon.Lichtenauer@figment.io>
2025-01-28 13:57:58 +0100

    fix axelar evm query path (#8)
    Signed-off-by: Simon Lichtenauer <Simon.Lichtenauer@figment.io>

--- internal/packages/duty/axelar-evm/types/types.go
+++ internal/packages/duty/axelar-evm/types/types.go
@@ -9,7 +9,7 @@ const (
 	CommonValidatorQueryPath = "/cosmos/staking/v1beta1/validators?status=BOND_STATUS_BONDED&pagination.count_total=true&pagination.limit=500"
 
 	// axelar
-	AxelarEvmChainsQueryPath        = "axelar/evm/v1beta1/chains?status=1"
+	AxelarEvmChainsQueryPath        = "/axelar/evm/v1beta1/chains?status=1"
 	AxelarChainMaintainersQueryPath = "/axelar/nexus/v1beta1/chain_maintainers/{chain}"
 )
 
